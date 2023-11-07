dd84f33db645b110d3616deec891dd5913c84a27
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-07 15:28:43 +0100

    created with function

--- js/script.js
+++ js/script.js
@@ -6,15 +6,18 @@ createApp({
         }
     },
     methods: {
+        getsEmail(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
+                console.log(resp.data.response)
+                this.email.push(resp.data.response)
+            })
+            }
+        }
 
     },
-    mounted() {
-        for(let i = 0; i < 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
-            console.log(resp.data.response)
-            this.email.push(resp.data.response)
-        })
-        }
+    created() {
+        this.getsEmail()
     }
 
 }).mount('#app')
