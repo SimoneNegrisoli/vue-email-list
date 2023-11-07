ec232f33ff1ea89ee2a1c322130e366438c252c3
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-07 14:54:44 +0100

    email

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@ const {createApp} = Vue;
 createApp({
     data (){
         return{
-
+            email: []
         }
     },
     methods: {
 
     },
+    mounted() {
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
+            console.log(resp.data.response)
+            this.email.push(resp.data.response)
+        })
+        }
+    }
 
 }).mount('#app')
